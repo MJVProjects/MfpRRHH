49aa57377d024407fa57d1ba7682107af3a26f29
BONATTI <dbonatti@ucvvirtual.edu.pe>
2022-05-19 19:18:24 -0500

    version 3 - DBP

--- index.php
+++ index.php
@@ -4,24 +4,33 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>MFP Recursos Humanos</title>
+    <!-- estilos globales -->
     <link rel="stylesheet" href="css/style.css">
     <!-- Boostrap -->
     <link href="libs/bootstrap/css/bootstrap.min.css" rel="stylesheet" >
+    <!-- header -->
+    <link rel="stylesheet" href="css/header.css">
     <!-- Vision Mision -->
     <link rel="stylesheet" href="css/mv.css">
     <!-- Carrucel -->
     <link rel="stylesheet" href="css/carousel.css">
     <link rel="stylesheet" href="css/carousel.rtl.css">
+    <!-- nosotros -->
+    <link rel="stylesheet" href="css/nosotros.css">
 
 
   </head>
   <body>
+
      <?php include_once 'views/header.html' ?>
       
     <?php include_once 'views/carrucel.html' ?>
 
     
     <?php include_once 'views/mv.html'?>
+
+
+    <?php include_once 'views/nosotros.html' ?>
 
     
      <main>
@@ -36,31 +45,7 @@
       
         <div class="container marketing">
       
-          <!-- Three columns of text below the carousel -->
-          <div class="row">
-            <div class="col-lg-4">
-              <svg class="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 140x140" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#777"/><text x="50%" y="50%" fill="#777" dy=".3em">140x140</text></svg>
-      
-              <h2 class="fw-normal">Heading</h2>
-              <p>Some representative placeholder content for the three columns of text below the carousel. This is the first column.</p>
-              <p><a class="btn btn-secondary" href="#">View details &raquo;</a></p>
-            </div><!-- /.col-lg-4 -->
-            <div class="col-lg-4">
-              <svg class="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 140x140" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#777"/><text x="50%" y="50%" fill="#777" dy=".3em">140x140</text></svg>
-      
-              <h2 class="fw-normal">Heading</h2>
-              <p>Another exciting bit of representative placeholder content. This time, we've moved on to the second column.</p>
-              <p><a class="btn btn-secondary" href="#">View details &raquo;</a></p>
-            </div><!-- /.col-lg-4 -->
-            <div class="col-lg-4">
-              <svg class="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 140x140" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#777"/><text x="50%" y="50%" fill="#777" dy=".3em">140x140</text></svg>
-      
-              <h2 class="fw-normal">Heading</h2>
-              <p>And lastly this, the third column of representative placeholder content.</p>
-              <p><a class="btn btn-secondary" href="#">View details &raquo;</a></p>
-            </div><!-- /.col-lg-4 -->
-          </div><!-- /.row -->
-      
+        
       
           <!-- START THE FEATURETTES -->
       
@@ -112,6 +97,9 @@
       
         <!-- FOOTER -->
        <?php include_once 'views/footer.html' ?>
+
+
+
       </main>
     <script src="libs/bootstrap/js/bootstrap.min.js"></script>
 
