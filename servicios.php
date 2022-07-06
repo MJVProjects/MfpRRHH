620d1692ab242e2ace588efb3ca37f7e9a034a2b
BONATTI <dbonatti@ucvvirtual.edu.pe>
2022-07-05 21:16:08 -0500

    DBP-05/07/2022

--- servicios.php
+++ servicios.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>MFP Recursos Humanos</title>
+    <link rel="icon" href="./img/icono.jpg">
     <!-- estilos globales -->
     <link rel="stylesheet" href="css/style.css">
     <!-- Boostrap -->
